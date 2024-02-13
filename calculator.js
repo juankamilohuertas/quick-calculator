3a5a6fa4f13c9d9351c0896b048fe5f8ea65a487
juan camilo huertas <juankamilohuertas2021@gmail.com>
2024-02-13 16:50:27 +0100

    menu de prueba

--- calculator.js
+++ calculator.js
@@ -1,177 +1,59 @@
 "use strict";
-const entrada1 = document.querySelector("#entrada");
-const mensaje = document.querySelector("p");
-const resDom = document.querySelector("div");
-/* pidiendo datos para crear el usuario */
-let infoUser = new Object();
-/* preguntando el nombre */
-let iterrador = 0;
-const registro = (entrada, answerEdad, answerHabilidades) => {
-  entrada1.value = "";
-  if (iterrador == 0) answerEdad("¿Cual es tu edad?", entrada);
-  else if (iterrador == 1)
-    answerHabilidades("selecciona cuales son tus habilidades", entrada);
-  else if (iterrador == 2) {
-    infoUser.habilidades = entrada;
-    info(infoUser.nombre, infoUser.edad, infoUser.habilidades);
-    atributoBoton();
-  }
-  iterrador++;
-};
-/* preguntando la edad */
-const edad = (edad, entrada) => {
-  infoUser.nombre = entrada;
-  mensaje.innerHTML = edad;
-};
-const habilidades = (answerhabilidad, entrada) => {
-  infoUser.edad = entrada;
-  mensaje.innerHTML = answerhabilidad;
-};
-/* atributos del botton */
-const atributoBoton = () => {
-  if (iterrador <= 1) {
-    mensaje.innerHTML = "¿Como te llamas?";
-    entrada1.type = "text";
-    entrada1.value = "";
-    entrada1.placeholder = "Escribe aqui";
-  } else {
-    mensaje.innerHTML = `bienbenid@ ${infoUser.nombre}`;
-    entrada1.type = "button";
-    entrada1.value = "NEXT";
-    entrada1.placeholder = "";
-  }
-};
-entrada1.addEventListener("mousedown", atributoBoton);
+import { data } from "./datos.js";
+const fragment = document.createDocumentFragment();
+const conteItems = document.querySelector("ul");
+const informacion = document.querySelector("div");
 
-entrada1.addEventListener("keydown", (e) => {
-  if (e.key == "Enter" && iterrador <= 2) {
-    registro(entrada1.value, edad, habilidades);
-  }
-});
-/* mostrando info */
-const info = (nombre, edad, habilidades) => {
-  const dom = `
-  User: ${nombre}
-  Edad: ${edad}
-  Habilidades: ${habilidades}`;
-  resDom.innerHTML = dom;
-};
+let nombreItems = ["Home", "Courses", "Blog", "Online", "The most recent"];
 
-/******************************************************/
-
-const encabezado = document.querySelector("h4");
-const entrada2 = document.querySelector("#entrada2");
-const mensaje2 = document.querySelector("ul");
-
-let contador = 0;
-const principal2 = () => {
-  item1(entrada2.value);
-  if (contador == 0) encabezado.innerHTML = "¿Que edad tienes?";
-  else if (contador == 1) encabezado.innerHTML = "¿que te gusta hacer?";
-  else if (contador >= 2) info2(data2);
-  contador++;
-};
-
-const item1 = (info) => {
-  data2[contador] = info;
-  entrada2.value = "";
-};
-
-entrada2.addEventListener("mousedown", () => {
-  if (contador == 0) {
-    entrada2.type = "text";
-    entrada2.value = "";
-    entrada2.placeholder = "Escribe aqui";
-    encabezado.innerHTML = "¿Como te llamas?";
-  }
+nombreItems.forEach((item) => {
+  let items = document.createElement("LI");
+  items.innerHTML = item;
+  fragment.appendChild(items);
+  conteItems.appendChild(fragment);
 });
 
-entrada2.addEventListener("keydown", (e) => {
-  if (e.key == "Enter") {
-    principal2();
-  }
-});
-
-let data2 = [];
-/* mostrando info */
-const info2 = (datos) => {
-  let dom = `
-  <li>Nombre: ${datos[0]}</li>
-  <li>Edad: ${datos[1]}</li>
-  <li>Pasatiempos: ${datos[2]}</li>
-  `;
-  mensaje2.innerHTML = dom;
-  entrada2.type = "button";
-  entrada2.value = "NEXT";
-  entrada2.removeAttribute("placeholder");
-};
-
-/******************************************************/
-
-const encabezado3 = document.querySelector("h5");
-const entrada3 = document.querySelector("#entrada3");
-const mensaje3 = document.querySelector("code");
-
-class Persona {
-  constructor(nombre, edad, habilidades) {
-    this.nombre = nombre;
-    this.edad = edad;
-    this.habilidades = habilidades;
-  }
-  botonEntrada() {
-    encabezado3.innerHTML = this.nombre;
-    entrada3.type = "text";
-    entrada3.placeholder = "Escribe aqui";
-    entrada3.removeAttribute("value");
-  }
-  botonSiguiente() {
-    encabezado3.innerHTML = `Bienvenida/o ${data.nombre}`;
-    entrada3.type = "button";
-    entrada3.removeAttribute("placeholder");
-    entrada3.value = "Next";
-  }
-  preguntasYrespuestas(e) {
-    if (e.key == "Enter" && contando <= 2) {
-      if (contando == 0) {
-        encabezado3.innerHTML = this.edad;
-        data.nombre = entrada3.value;
-      } else if (contando == 1) {
-        encabezado3.innerHTML = this.habilidades;
-        data.edad = entrada3.value;
-      } else if (contando == 2) {
-        data.habilidades = entrada3.value;
-      }
-      entrada3.value = "";
-      contando++;
+for (let i = 0; i < nombreItems.length; i++) {
+  conteItems.children[i].addEventListener("click", () => {
+    for (const i of conteItems.children) i.style.background = "transparent";
+    conteItems.children[i].style.background = "#f00";
+    informacion.innerHTML = "";
+    switch (conteItems.children[i].textContent) {
+      case "Home":
+        informacion.innerHTML = `<img width=100 src ="https://s9.postimg.cc/ljpnkxxe7/online.jpg" alt="imagen de pagina de inicio">`;
+        break;
+      case "Courses":
+        data.cursos.curso.forEach((e) => {
+          informacion.innerHTML += `
+          <h1>${e}</h1>
+          <p>${data.cursos.descripcion}</p>
+          <aside>${data.cursos.infor}</aside>
+          `;
+        });
+        break;
+      case "Blog":
+        informacion.innerHTML = `
+          <img width=100 src=${data.blog.img} alt="imagen blog">
+          <p>${data.blog.descripcion}</p>
+          <aside>${data.blog.infor}</aside>
+          `;
+        break;
+      case "Online":
+        informacion.innerHTML = `
+          <img width=100 src=${data.online.img} alt="imagen de sesion online">
+          <p>${data.online.descripcion}</p>
+          <aside>${data.online.infor}</aside>
+          `;
+        break;
+      case "The most recent":
+        for (const i of data.recent.img) {
+          informacion.innerHTML += `
+          <img width=100 src=${i} alt="imagenes de pagina recent">
+          <p>${data.recent.descripcion}</p>
+          <aside>${data.recent.infor}</aside>
+          `;
+        }
+        break;
     }
-  }
-  infoDom() {
-    if (contando == 3) {
-      mensaje3.innerHTML = `
-      Nombre: ${data.nombre}
-      Edad: ${data.edad}
-      Habilidades: ${data.habilidades}`;
-      this.botonSiguiente();
-    }
-  }
+  });
 }
-
-const preguntas = new Persona(
-  "¿Como te llamas?",
-  "¿Cual es tu edad?",
-  "¿Que te gusta hacer?"
-);
-
-let contando = 0;
-let data = new Persona();
-
-entrada3.addEventListener("mousedown", () => {
-  if (!contando) preguntas.botonEntrada();
-});
-
-entrada3.addEventListener("keydown", (e) => {
-  preguntas.preguntasYrespuestas(e);
-  preguntas.infoDom();
-});
-
-
---
+++ datos.js
@@ -0,0 +1,42 @@
+let data = {
+  cursos: {
+    curso: ["HTML", "CSS", "JAVASCRIPT", "REACT", "NODE.JS", "SQL"],
+    descripcion: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nisi soluta mollitia ipsam autem quasi modi, quia fuga minus unde illum fugiat laboriosam provident similique qui corrupti? Dignissimos, repudiandae? Consequuntur!
+    Voluptatibus laborum inventore fugiat quos, tempore soluta ex, veniam aut atque saepe quae commodi quibusdam ipsum! Veniam earum vero a vitae magnam quis aliquam cum maxime. In corrupti quos ad.
+    Veritatis ut error enim, ad, in explicabo tempore eum neque atque voluptate aperiam excepturi. Nulla, cupiditate culpa similique consectetur ullam atque eos adipisci rerum officiis dolorem, eius nihil expedita voluptate?
+    Dolore soluta incidunt, praesentium voluptates earum laudantium a dignissimos facere fugiat voluptas molestiae accusamus veniam maiores quisquam quasi nobis labore aliquam vitae rerum ducimus, aperiam eaque obcaecati. Odit, magnam dolorum.`,
+    infor: "mas informacion",
+  },
+
+  blog: {
+    img: "https://storage.googleapis.com/www-paredro-com/uploads/2019/10/97c703a6-bigstock-web-design-and-coding-in-inter-299285242.jpg",
+    descripcion: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nisi soluta mollitia ipsam autem quasi modi, quia fuga minus unde illum fugiat laboriosam provident similique qui corrupti? Dignissimos, repudiandae? Consequuntur!
+    Voluptatibus laborum inventore fugiat quos, tempore soluta ex, veniam aut atque saepe quae commodi quibusdam ipsum! Veniam earum vero a vitae magnam quis aliquam cum maxime. In corrupti quos ad.
+    Veritatis ut error enim, ad, in explicabo tempore eum neque atque voluptate aperiam excepturi. Nulla, cupiditate culpa similique consectetur ullam atque eos adipisci rerum officiis dolorem, eius nihil expedita voluptate?
+    Dolore soluta incidunt, praesentium voluptates earum laudantium a dignissimos facere fugiat voluptas molestiae accusamus veniam maiores quisquam quasi nobis labore aliquam vitae rerum ducimus, aperiam eaque obcaecati. Odit, magnam dolorum.`,
+    infor: "mas informacion",
+  },
+
+  online: {
+    img: "https://nestrategia.com/wp-content/uploads/2014/12/marketing-online-1.jpg",
+    descripcion: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nisi soluta mollitia ipsam autem quasi modi, quia fuga minus unde illum fugiat laboriosam provident similique qui corrupti? Dignissimos, repudiandae? Consequuntur!
+    Voluptatibus laborum inventore fugiat quos, tempore soluta ex, veniam aut atque saepe quae commodi quibusdam ipsum! Veniam earum vero a vitae magnam quis aliquam cum maxime. In corrupti quos ad.
+    Veritatis ut error enim, ad, in explicabo tempore eum neque atque voluptate aperiam excepturi. Nulla, cupiditate culpa similique consectetur ullam atque eos adipisci rerum officiis dolorem, eius nihil expedita voluptate?
+    Dolore soluta incidunt, praesentium voluptates earum laudantium a dignissimos facere fugiat voluptas molestiae accusamus veniam maiores quisquam quasi nobis labore aliquam vitae rerum ducimus, aperiam eaque obcaecati. Odit, magnam dolorum.`,
+    infor: "mas informacion",
+  },
+
+  recent: {
+    img: [
+      "https://www.initcoms.com/wp-content/uploads/2016/11/estrategias-para-conseguir-primera-venta-online-2.png",
+      "https://static.doofinder.com/main-files/uploads/2020/05/vender-una-tienda-online.jpg",
+      "https://www.distancelearningcollege.co.uk/wp-content/uploads/2022/02/Online-learning-scaled.jpg",
+    ],
+    descripcion: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nisi soluta mollitia ipsam autem quasi modi, quia fuga minus unde illum fugiat laboriosam provident similique qui corrupti? Dignissimos, repudiandae? Consequuntur!
+    Voluptatibus laborum inventore fugiat quos, tempore soluta ex, veniam aut atque saepe quae commodi quibusdam ipsum! Veniam earum vero a vitae magnam quis aliquam cum maxime. In corrupti quos ad.
+    Veritatis ut error enim, ad, in explicabo tempore eum neque atque voluptate aperiam excepturi. Nulla, cupiditate culpa similique consectetur ullam atque eos adipisci rerum officiis dolorem, eius nihil expedita voluptate?
+    Dolore soluta incidunt, praesentium voluptates earum laudantium a dignissimos facere fugiat voluptas molestiae accusamus veniam maiores quisquam quasi nobis labore aliquam vitae rerum ducimus, aperiam eaque obcaecati. Odit, magnam dolorum.`,
+    infor: "mas informacion",
+  },
+};
+export { data };
